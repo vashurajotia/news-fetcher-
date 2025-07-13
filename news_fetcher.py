47655a39691356d09e2ee069426d833115ad09b3
vashu rajotia <166683954+vashurajotia@users.noreply.github.com>
2025-07-13 13:06:09 +0530

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,1 +1,15 @@
+import requests
 
+query = input("What type of news are you interested in today? : ")
+api = "86e5b73b9bfd4c7da3677ff20a50354c"
+
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-06-13&sortBy=publishedAt&apiKey={api}"
+
+r = requests.get(url)
+print(r.json())
+
+data = r.json()
+articles = data["articles"]
+for index, article in enumerate(articles):
+    print(index+1, article["title"], article["url"])
+    print("\n******************************************************************************************\n")
